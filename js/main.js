cb6cd8fbb596d509e6eaa789d947b4f51370d9a1
Ritvik19 <rastogiritvik99@gmail.com>
2019-06-04 14:57:46 +0530

    Date Picker

--- js/main.js
+++ js/main.js
@@ -4,6 +4,8 @@ var d = new Date();
 
 var currentDate = d.getDate();
 var currentMonth = d.getMonth();
+var calDate = d.getDate();
+var calMonth = d.getMonth();
 
 function title(str) {
   return str.replace(/(^|\s)\S/g, function(t) { return t.toUpperCase() });
@@ -27,10 +29,62 @@ function parse2HTML(years, events)
   return HTMLcontent
 }
 
+function makeCalender(month, date)
+{
+  var days = 29;
+  if(month == 0 || month == 2 || month == 4 || month == 6 || month == 7 || month == 12 || month == 11)
+  {
+    days = 31;
+  }
+  else if (month == 3 || month == 5 || month == 8 || month == 10)
+  {
+    days = 30;
+  }
+  calenderContent = "<tr>";
+  for(i=1; i<=days; i++)
+  {
+    if(i == date && month==currentMonth)
+    {
+      calenderContent += '<td class="w3-black w3-center" onclick="fetchData('+month+', '+i+')">'+i+'</td>';
+    }
+    else
+    {
+      calenderContent += '<td class="w3-center w3-hover-dark-gray" onclick="fetchData('+month+', '+i+')">'+i+'</td>';
+    }
+    if(i%7 == 0)
+    {
+      calenderContent += '</tr><tr>';
+    }
+  }
+  calenderContent += '</tr>';
+  return calenderContent;
+}
+
+function prevMonth()
+{
+  calMonth = (calMonth+11)%12;
+  document.getElementById('month').innerHTML = title(monthMapping[calMonth]);
+  document.getElementById('calender').innerHTML = makeCalender(calMonth, calDate);
+
+}
+
+function nextMonth()
+{
+  calMonth = (calMonth+1)%12;
+  document.getElementById('month').innerHTML = title(monthMapping[calMonth]);
+  document.getElementById('calender').innerHTML = makeCalender(calMonth, calDate);
+}
+
 function fetchData(month, date)
 {
+  currentDate = date;
+  currentMonth = month;
+  calMonth = month;
+  calDate = date;
   document.getElementById('date').innerHTML = title(monthMapping[month]) + ' ' + date;
   document.getElementById('date-bottom').innerHTML = title(monthMapping[month]) + ' ' + date;
+  document.getElementById('month').innerHTML = title(monthMapping[calMonth]);
+  document.getElementById('calender').innerHTML = makeCalender(calMonth, calDate);
   var xhttp = new XMLHttpRequest();
   xhttp.onreadystatechange = function() {
     if (this.readyState == 4 && this.status == 200) {
